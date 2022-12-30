ef7956caf707b77abbd02cc4592c6a0fb63abf2e
U-DELTA\I-Ritesh.K <I-Ritesh.K@INBLRNB0056.delta.corp>
2022-12-30 15:19:16 +0530

    modified bmp180_main.c

--- bmp180/bmp180_main.c
+++ bmp180/bmp180_main.c
@@ -45,7 +45,63 @@ static const char *TAG = "main";
 #define ID							0xD0
 /*calib21 to calib 0                  0xBF down to 0xAA*/
 
+static esp_err_t i2c_master_init()
+{
+	int i2c_master_port = I2C_MASTER_NUM;
+	i2c_config_t conf;
+	conf.mode - I2C_MODE_MASTER;
+	conf.sda_io_num = I2C_MASTER_SDA_IO;
+	conf.sda_pullup_en = 1;
+	conf.scl_io_num = I2C_MASTER_SCL_IO;
+	conf.scl_pullup_en = 1;
+	conf.clk_stretch_tick = 300; // 300 ticks, Clock stretch is about 210us, you can make changes according to the actual situation.
+	ESP_ERROR_CHECK(i2c_driver_install(i2c_master_port, conf.mode));
+	ESP_ERROR_CHECK(i2c_param_config(i2c_master_port, &conf));
+	return ESP_OK;
+}
 
+//code to write to bmp180
+
+static esp_err_t i2c_master_bmp180_write(i2c_port_t i2c_num, uint8_t reg_address, uint8_t *data, size_t data_len)
+{
+	int ret;
+	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, BMP180_ADDR << 1 | WRITE_BIT, ACK_CHECK_EN);
+	i2c_master_write_byte(cmd, reg_address, ACK_CHECK_EN);
+	i2c_master_write(cmd, data, data_len, ACK_CHECK_EN);
+	i2c_master_stop(cmd);
+	ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+	i2c_cmd_link_delete(cmd);
+
+	return ret;
+}
+
+static esp_err_t i2c_master_bmp180_read(i2c_port_t i2c_num, uint8_t reg_address, uint8_t *data, size_t data_len)
+{
+	int ret;
+	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, BMP180_ADDR << 1 | WRITE_BIT, ACK_CHECK_EN);
+	i2c_master_write_byte(cmd, reg_address, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+
+	if (ret != ESP_OK)
+	{
+		return ret;
+	}
+	md = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, BMP180_ADDR << 1 | READ_BIT, ACK_CHECK_EN);
+    i2c_master_read(cmd, data, data_len, LAST_NACK_VAL);
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+
+	return ret;
+}
 void app_main() {
 	xTaskCreate(temperature_task, "temperature task", 2048, NULL,
 			tskIDLE_PRIORITY, NULL);
