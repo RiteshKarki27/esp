62d3d6cb47d9c23177f0d3c7a5c616ae70eebdfe
U-DELTA\Ritesh.S <Ritesh.S@INBLRNB0056.delta.corp>
2023-02-21 12:44:46 +0530

    modified version of thingspeak

--- thingspeak_upload/thingspeak_main.c
+++ thingspeak_upload/thingspeak_main.c
@@ -12,13 +12,16 @@
 #include "esp_err.h"
 
 #include "driver/i2c.h"
+#include "bmp180/bmp180_main.c"
+#include "dht11/dht11_main.c"
+
 
 
 const char* host = "api.thingspeak.com"; 
 const int httpPort = 80;
-const String channelID   = "UNIJETI SVOJ"; 
-const String writeApiKey = "UNIJETI SVOJ";
-const String readApiKey = "Unijeti svoj ";
+const String channelID   = ""; 
+const String writeApiKey = "";
+const String readApiKey = "";
 int field1 = 0;
 const float BETA = 3950;
 int numberOfResults = 3; 
@@ -31,11 +34,11 @@ static void setup_wifi()
   WiFi.begin(ssid,password);
   while(WiFi.status() != WL_CONNECTED) 
   {
-    Serial.println("Spaja se!!!");
+    Serial.println("Connecting");
     delay(1000);
   }
   Serial.println("Connected!!!");
-  Serial.println("Ip_adresa: ");
+  Serial.println("Ip Address: ");
   Serial.print(WiFi.localIP());
 }
 void setup() 
@@ -46,24 +49,24 @@ void setup()
 }
 static void send_to_cloud(WiFiClient *client)  
 {
-  unsigned long proteklo_t = millis();
+  unsigned long time_passed = millis();
 
   while(client->available() == 0) 
   {
-    if(millis() - proteklo_t >= 5000)  
+    if(millis() - time_passed >= 5000)  
     {
-      Serial.println("Time out!!!!!");
+      Serial.println("Time out\n");
       client->stop();
       return;
     }
   }
   while(client->available()) 
   {
-    String za_ispis=client->readStringUntil('\r');
-    Serial.println(za_ispis);
+    String string_t=client->readStringUntil('\r');
+    Serial.println(string_t);
   }
 }
-void loop() 
+void thingspeak_main_task() 
 {
   WiFiClient client; 
   int analogValue=analogRead(34); 
@@ -94,4 +97,9 @@ void loop()
   client.print(readRequest);
   send_to_cloud(&client);
   delay(60000); 
+}
+
+void app_main() {
+	xTaskCreate(thingspeak_main_task, "thingspeak_main_task task", 2048, NULL,
+			tskIDLE_PRIORITY, NULL);
 }
